2adda6e41efce584befaa61bc5f09d4036ad5b8d
moinabyssinia <michaelg.tadesse@gmail.com>
2020-09-07 21:10:45 -0400

    added first number - debug operator functions

--- script.js
+++ script.js
@@ -16,13 +16,25 @@ const operate = function(operator, a, b){
 }
 
 //add listener for buttons
-const showButtons = document.querySelectorAll(".show");
-const screenValue = document.getElementsByClassName('screen');
+const clickable = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
 
-let 
+let numberSilo = {
+    firstNumber: [],
+    secondNumber: []
+};
 
-showButton.forEach(element, function(element){
+clickable.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        numberSilo.firstNumber.push(btn.textContent);
+        console.log(numberSilo.firstNumber.join(''));
+    });
+  });
+
+
+//find a way to call the four functions from one function here
+operators.forEach(function(element){
     element.addEventListener('click', function(){
-        
+        numberSilo.firstNumber = Number(numberSilo.firstNumber.join(''));
     })
 })
